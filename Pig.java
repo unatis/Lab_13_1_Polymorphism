e1a8c3f378a46e96dc6b030a953b8a154cc1b729
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-07 16:59:56 +0200

    Create Pig.java

---
+++ Pig.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Pig extends Animal{
+
+    public String PigType = "";
+
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
