0966b7ce658733968dbe0cdd71014021a79ed362
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-07 16:59:37 +0200

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Animal {
+
+    public String AnimalName = "";
+
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
