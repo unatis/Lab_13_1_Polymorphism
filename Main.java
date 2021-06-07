1ba87d81cace8547ced2ad12cf35b0a53697b530
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-07 16:59:17 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal myAnimal = new Animal(); // Create a Animal object
+        Animal myPig = new Pig();  // Create a Pig object
+        Animal myDog = new Dog();  // Create a Dog object
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+
+    }
+}
