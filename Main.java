1fb290f2451456f4dee084ce7f5110ea7e95b7b7
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-14 17:31:50 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal myAnimal = new Animal(); // Create a Animal object
-        Animal myPig = new Pig();  // Create a Pig object
-        Animal myDog = new Dog();  // Create a Dog object
-
-        myAnimal.animalSound();
-        myPig.animalSound();
-        myDog.animalSound();
+        Animal animal = new Animal();
+        animal.animalSound();
+        
+        Pig pig = new Pig();
+        pig.animalSound();
+        
+        Dog dog = new Dog();
+        dog.animalSound();
 
     }
 }
